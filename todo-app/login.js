8f8e914b68132d543493fe3ea85daa123a421de0
Sarawar Hossain <sh-riyad>
2024-06-21 23:21:57 +0600

    todo complete

--- todo-app/login.js
+++ todo-app/login.js
@@ -11,43 +11,80 @@ const usernameInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
 const alert = document.querySelector("#alert");
 
-loginButton.addEventListener("click", (event) => {
-    event.preventDefault();
+let currentUser = localStorage.getItem("currentUser") || "None";
 
-    const username = usernameInput.value.trim();
-    const password = passwordInput.value.trim();
-
-    if (username == "" || password == "") {
-        showAlert("Username or password cannot be empty");
-    }else if (usernames.includes(username)) {
-        if (username === password) {
-            showAlert("Login successfull");
-
-            localStorage.setItem("username", username)
-
-            setTimeout(() => {
-                window.location.href = 'index.html';
-            }, 1000);
-
-        } else {
-            showAlert("Invalid password");
-        }
+// check if the user is already logged in or not
+function initialState(){
+    if (currentUser == "None") {
+        renderLogin();
     } else {
-        showAlert("Invalid username");
+        window.location.href = 'index.html';
     }
-});
+}
 
-function showAlert(message){
+
+// verify email password and execute login operation
+function renderLogin() {
+    loginButton.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        // Take usename and password from user
+        const username = usernameInput.value.trim();
+        const password = passwordInput.value.trim();
+
+
+        if (username == "" || password == "") {
+            showAlert("Username or password cannot be empty");
+        }else if (usernames.includes(username)) {
+            if (username === password) {
+                showAlert("Login successfull", "green");
+                localStorage.setItem("currentUser", username) // update localstorage
+
+                // wait 1 sec before login
+                setTimeout(() => {
+                    window.location.href = 'index.html';
+               }, 1000);
+
+            } else {
+                showAlert("Invalid password","red");
+            }
+        } else {
+            showAlert("Invalid username","red");
+        }
+    });
+    
+}
+
+
+// These functions will show the alert message
+function showAlert(responseMessage, status){
     alert.classList.remove("hidden");
-    let alertMessage = alert.querySelector("#alertMessage");
-    alertMessage.textContent = message;
+    let svg;
+    if (status == "red") {
+       svg = document.querySelector("#errorSVG");
+       svg.classList.remove("hidden");
+       alert.classList.add("text-red-800", "bg-red-300");
+    } else {
+       svg = document.querySelector("#successSVG");
+       svg.classList.remove("hidden");
+       alert.classList.add("text-green-800", "bg-green-300");
+    }
+   let alertMessage = alert.querySelector("#alertMessage");
+   alertMessage.textContent = responseMessage;
 
-    const button = alert.querySelector("button");
-    button.addEventListener("click", hideAlert);
+   const button = alert.querySelector("button");
+   button.addEventListener("click", () => hideAlert(svg));
 
-    setTimeout(hideAlert, 5000);
+    setTimeout(() => hideAlert(svg), 5000);
 }
 
-function hideAlert() {
+function hideAlert(svg) {
     alert.classList.add("hidden");
-}
+    alert.classList.remove("text-red-800", "bg-red-300", "text-green-800", "bg-green-300");
+    svg.classList.add("hidden");
+ }
+
+
+initialState()
+renderLogin()
+
